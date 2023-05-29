9c09aeea6ca5643336d838d4dc2412c9ffbf84a7
lambda-workstation <jatnikonm@binusian.org>
2023-05-29 14:03:13 +0700

    update

--- examples/data/aml.py
+++ examples/data/aml.py
@@ -2,4 +2,12 @@
 import pandas as pd
 
 # membaca dataframe dari file csv
-dataframe = pd.read_csv('/home/dfai/DataspellProjects/APUChat/examples/data/LI-Small_Trans.csv')
+#dataframe = pd.read_csv('/home/lambda_one/DataspellProjects/pandas-ai/examples/data/HI-Small_Trans.csv')
+dataframe = pd.read_csv('/home/lambda_one/DataspellProjects/pandas-ai/examples/data/500k.csv')
+
+# lokasi data di workstation  
+#/home/lambda_one/DataspellProjects/pandas-ai/examples/data/HI-Small_Trans.csv
+
+
+# lokasi data di laptop 
+# /home/dfai/DataspellProjects/APUChat/examples/data/LI-Small_Trans.csv
